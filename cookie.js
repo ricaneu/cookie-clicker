91a61d5d1e7f8831f9266e654c2642e24dce830f
ricaneu <79845716+ricaneu@users.noreply.github.com>
2021-07-14 20:45:13 +0200

    up

--- cookie.js
+++ cookie.js
@@ -1,13 +1,47 @@
 var cookie;
 
+var coincount = 0;
+var counter = 0;
 var cookieCount = 0;
-var cookie = document.getElementByGetElementById("cookie");
-cookie.onclick = function() {
-    cookieCount++;
-    alert(cookieCount);
-    countElement.innerHTML = cookieCount;
+
+
+var cookie = document.getElementById("cookie");
+
+
+function update(c, d) {
+    var countCoins = document.getElementById("coins");
+    var countElement = document.getElementById("count");
+    countCoins.innerHTML = d
+    countElement.innerHTML = c;
 }
 
-var cps = 0;
-var cursorCount = 0;
-var cursorCost = 0;
+
+
+cookie.onclick = function() {
+    cookieCount++;
+    counter = counter + 1;
+    //    countCoins.==++innerHTML = Math.floor(cookieCount / 10);
+    if (counter == 10) {
+        coincount++;
+        counter = 0;
+    }
+    update(cookieCount, coincount)
+}
+
+
+function buy() {
+    if (coincount > 0) {
+        coincount--
+        cookieCount = cookieCount + 100
+        update(cookieCount, coincount)
+    }
+}
+
+
+function reset() {
+    coincount = 0;
+    counter = 0;
+    cookieCount = 0;
+
+    update(0, 0)
+}
